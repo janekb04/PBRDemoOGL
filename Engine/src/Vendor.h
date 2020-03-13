11d3ede7037ba324d436f0645d831f2aa0c1d1ff
Y B <janekb04@icloud.com>
2020-03-13 11:07:27 +0100

    Add libreries

---
+++ Engine/src/Vendor.h
@@ -0,0 +1,43 @@
+#pragma once
+
+//IMGUI
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+//GLEW
+#include <Gl/glew.h>
+//GLFW
+#include <GLFW/glfw3.h>
+//GLM
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/ext/quaternion_trigonometric.hpp>
+#include <glm/gtx/quaternion.hpp>
+#include <glm/gtc/type_ptr.hpp>
+//STB
+#include <stb/stb_image.h>
+//ASSIMP
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+//CSTDLIB
+#include <cstdio>
+#include <cstdlib>
+//STL
+#include <memory>
+#include <unordered_set>
+#include <list>
+#include <vector>
+#include <functional>
+#include <unordered_map>
+#include <thread>
+#include <variant>
+#include <mutex>
+#include <array>
+#include <forward_list>
+//IO
+#include <iostream>
+#include <fstream>
