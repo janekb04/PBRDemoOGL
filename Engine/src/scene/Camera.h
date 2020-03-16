71de16022aad8acebec052cde7486d4bb0d89263
Y B <janekb04@icloud.com>
2020-03-16 21:00:20 +0100

    Moved files around, added World class

---
+++ Engine/src/scene/Camera.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "../Vendor.h"
+#include "../Viewport.h"
+#include "../Transform.h"
+
+struct PerspectiveCamera
+{
+	Transform transform;
+	float fov = 60.0f;
+	float nearPlane = 0.3f;
+	float farPlane = 1000.0f;
+
+	glm::mat4 get_projection_matrix(const Viewport& viewport) const
+	{
+		if (farPlane == std::numeric_limits<float>::infinity())
+			return glm::infinitePerspective(fov, viewport.aspect<float>(), farPlane);
+		else
+			return glm::perspective(glm::radians(fov), viewport.aspect<float>(), nearPlane, farPlane);
+	}
+	glm::mat4 get_view_matrix() const
+	{
+		return transform.to_mat4();
+	}
+};
---
+++ Engine/src/scene/World.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "../Vendor.h"
+#include "Mesh.h"
+#include "../gl/VertexArray.h"
+#include "../gl/Buffer.h"
+#include "../gl/Texture2d.h"
+
+class World
+{
+private:
+	VertexArray VAO;
+	Buffer VBO, EBO, draw_indirect;
+public:
+	World( /*...*/ )
+	{
+
+	}
+};
